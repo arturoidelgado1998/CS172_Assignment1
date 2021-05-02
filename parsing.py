2970b54215e2efc937ae592d77175a0f58f6d974
arturoidelgado1998 <59622073+arturoidelgado1998@users.noreply.github.com>
2021-05-01 21:33:29 -0700

    added comments to each line of code

--- parsing.py
+++ parsing.py
@@ -48,7 +48,7 @@ def result(command):# Regular expressions to extract data from the corpus
                 #removes the underscore here
                 newText = text.replace("_"," ")
                 
-                #removing 
+                #Using the finditer to implement the regex. Finds the start of each word that fits and end. adds it to tokenization list
                 for word in re.finditer(token_regex,newText):
                     begin = word.start()
                     end = word.end()
@@ -58,35 +58,46 @@ def result(command):# Regular expressions to extract data from the corpus
                     #    text = text.replace(word , "")
                 #t = token_regex.sub('',text)
                 #cleaned_Text = t.split()
+
+                #the for loop stops each line and only grabs the word.
                 stopword_list = []
                 for line in stopwords_line:
                     stripped_word = line.rstrip("\n")
+                    #add the stop word to list
                     stopword_list.append(stripped_word)
                 
                 #for words in stopword_list:
                 #    if words in tokenization_list:
                 #        tokenization_list.remove(words)
+                
+                #makes a new tokenization list which compares the stopwords with the old tokenization list and removes stop words
                 tokenization_list = [i for i in tokenization_list if i not in stopword_list]
+                
                 term_id_list = []
                 position_count_list = []
                 position_count = 1
+                #makes a term id list by encoding it into binary of each word is the term_id
                 for words_stop in tokenization_list:
                     term_id_list.append(words_stop.encode())
                     position_count = position_count +1
                     position_count_list.append(position_count)
 
+                #for loop that runs from 0 to the size of the tokenization list
                 for i in range(0,len(tokenization_list)):
+                    #checks if the key which is a word is in the dictionary already. if it is then it appends the termid , doc id and position of the word
                     if(tokenization_list[i] in tuple_dictionary):
                         tuple_dictionary[tokenization_list[i]].append((term_id_list[i],doc_id_occurence,position_count_list[i]))
                     elif(tokenization_list[i] not in tuple_dictionary):
+                        #if its not then it adds the key with its first value which is the term id , doc id and position of the word
                         tuple_dictionary[tokenization_list[i]] = [(term_id_list[i],doc_id_occurence,position_count_list[i])]
 
+                #same as the loop above excepts adds the doc_id and number of words in the document
                 if(docno in doc_dictionary):
                     doc_dictionary[docno].append((doc_id_occurence,position_count_list[-1]))
-                
                 else:
+                    #checks if the list is empty
                     if position_count_list:
-                        #print(position_count_list)
+                        #adds the first key with values
                         doc_dictionary[docno] = [(doc_id_occurence,position_count_list[-1])]
                 
 
@@ -95,53 +106,69 @@ def result(command):# Regular expressions to extract data from the corpus
         #print(term_id_list)
                 doc_id_occurence = doc_id_occurence +1
     #print(doc_dictionary[command])
+
+    #if the command has --doc at position one then it will run
     if("--doc" == command[1]):
+        #command is broken down into a list of command that was inputted EX: [".\read_index.py", "--term", "human", "--doc", "AP890321-0001"]
+        #print command at 2 which should be the DOCNAME
         print("Listing for document: ",command[2])
+
+        #runs a loop that checks each value in the certain key and returns DOC in the tuple and the total position in the tuple
         for docid in doc_dictionary[command[2]]:
             print("DOCID: ",docid[0])
             print("Total Terms: ", docid[1])
 
+    #same as above except it checks if -- doc is not in the command
     if("--term" == command[1] and "--doc" not in command):
+        #prints the listings for term in the tuple
         print("listing for term: ", command[2])
+        #list for the first value in the tuple waste of memory and time ... i know
         first_value = []
         for tuple_shows in tuple_dictionary[command[2]]:
             first_value.append(tuple_shows[0])
+        
+        #Term ID which is the first value in the list
         print("TERMID:" ,first_value[0])
+        #checks the length of the values in the tuple to check the total amount of times the word is used
         print("Total:" ,len(tuple_dictionary[command[2]]))
+        #counter that starts at 0 and increments and the list Doc_count_terms checks if the doc id is already in the list
         counter2 = 0
         Doc_cont_terms = []
         for tuple_shown in tuple_dictionary[command[2]]:
+            #if doc id is not in the tuple then it increments and adds it else it goes to the next one
             if tuple_shown[1] not in Doc_cont_terms:
                 counter2 += 1
                 Doc_cont_terms.append(tuple_shown[1])
         print("Number of documents containing term: " ,counter2)
-        
+
+    #same as above but chekcs if --doc is in the command    
     if("--term" == command[1] and "--doc" in command ):
+        #command[2] is the term being used and command[4] is the document
         print("Inverted list for term:", command[2])
         print("In document: " , command[4])
         first_value2 = []
         term_freq_count = 0
+        #checks for the doc id number to be used later in the for loop
         for docid in doc_dictionary[command[4]]:
             doc_id_number = docid[0]
             #print(doc_id_number)
         position_intuple = []
+
+    
         for tuple_shown in tuple_dictionary[command[2]]:
             #print(tuple_shown[1])
+            #if the value tuple has the doc id then it increments
             if (tuple_shown[1]  == doc_id_number):
                 term_freq_count += 1
+                #it also adds the position of the word to the position_tuple list
                 position_intuple.append(tuple_shown[2])
+            #shows the term id and puts it in the list of first value
             first_value2.append(tuple_shown[0])    
         print("TERMID:" ,first_value2[0])
         print("DOCID: ",doc_id_number)
         print("Term frequency in document: ", term_freq_count)
         print("Positions: ", position_intuple)
-            
-
-
-    #print(counter2)
-        
-
-
+        #finally outputs   
             
             # step 3 - build index
             
